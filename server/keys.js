cd8f12e938425170ca7fa655e256a20e0a435e37
rashidmajeed <rashid.caliber@gmail.com>
2019-01-09 23:20:18 +0100

    rewrite enviornment variables for postgres credentials

--- server/keys.js
+++ server/keys.js
@@ -1,8 +1,7 @@
 module.exports = {
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT
+  pgUser: process.env.PG_USER,
+  pgHost: process.env.PG_HOST,
+  pgDatabase: process.env.PG_DATABASE,
+  pgPassword: process.env.PG_PASSWORD,
+  pgPort: process.env.PG_PORT
 };
-
